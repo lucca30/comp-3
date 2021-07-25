0e9d912d686e423add48048fef0e282352000df5
Lucca Martins <lucca_martins30@yahoo.com.br>
2021-07-25 12:21:26 -0300

    Setting up the enviroment

--- week-1/pilha.cc
+++ week-1/pilha.cc
@@ -1,56 +1,80 @@
+// ========================= Start - Code Session =========================
+#include <bits/stdc++.h>
 #include <iostream>
+#define assert(e) print_assert(e, #e, __FILE__, __LINE__)
+
+// ========================== End - Code Session ==========================
+
+// ========================= Start - Code Session =========================
+#define MAX_PILHA 10
 using namespace std;
+
 class PilhaInt
 {
 public:
     PilhaInt()
     {
         atual = 0;
+        for (int i = 0; i < MAX_PILHA; i++)
+        {
+            tab[i] = 0;
+        }
     }
-    void empilha(int valor)
+
+    PilhaInt &operator=(const PilhaInt &other)
     {
-        tab[atual++] = valor;
+        return *this;
     }
+
+    void empilha(int val)
+    {
+        tab[atual++] = val;
+    }
+
     int desempilha()
     {
         return tab[--atual];
     }
-    void print(std::ostream &o)
+
+    void print(ostream &output_stream, const char *msg)
     {
-        o << "[ ";
-        for (int i=0; i<this->atual;i++ )
-        {
-            o << this->tab[i];
-            if(i < this->atual-1) o << ", ";
-        }
-        o << " ]";
-    }
-    const PilhaInt &operator=(const PilhaInt &p)
-    {
-        atual = p.atual;
-        for (int i = 0; i < atual; i++)
-            tab[i] = p.tab[i];
-        return *this;
-    }
-    PilhaInt &operator<<(int valor)
-    {
-        empilha(valor);
-        return *this;
     }
 
 private:
+    int tab[MAX_PILHA];
     int atual;
-    int tab[1000];
 };
+
+// ========================== End - Code Session ==========================
+
+// ========================= Start - Debug Session =========================
+
+void print_assert(int success, const char *expression, const char *file, int line)
+{
+    if (success)
+    {
+        cout << "Passed!!" << endl;
+    }
+    else
+    {
+        cout << "Expression " << expression << " failed at " << line << " line." << endl;
+    }
+}
 
 int main()
 {
-    PilhaInt a;
-    a.empilha(1);
-    a.empilha(2);
-    a.empilha(3);
-    a.print(cout);
-    int x1 = a.desempilha();
-    int x2 = a.desempilha();
-    int x3 = a.desempilha();
+    PilhaInt p1;
+    p1.empilha(10);
+    assert(p1.desempilha() == 10);
+
+    PilhaInt p2;
+    p2.empilha(8);
+    p2.empilha(9);
+    p2.empilha(10);
+    assert(p2.desempilha() == 10);
+    assert(p2.desempilha() == 9);
+    assert(p2.desempilha() == 8);
+
 }
+
+// ========================== End - Debug Session ==========================
