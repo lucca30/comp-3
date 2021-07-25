e60c6c3b8dabf4b16a3dc1d7265bf7a5c6c55a8f
Lucca Martins <lucca_martins30@yahoo.com.br>
2021-07-25 15:16:13 -0300

    Finishing PilhaInt

--- week-1/pilha.cc
+++ week-1/pilha.cc
@@ -23,6 +23,17 @@ public:
 
     PilhaInt &operator=(const PilhaInt &other)
     {
+        this->atual = 0;
+        for (int i=0;i<other.atual;i++)
+        {
+            this->empilha(other.tab[i]);
+        }
+        return *this;
+    }
+
+    PilhaInt &operator<<(const int &other)
+    {
+        this->empilha(other);
         return *this;
     }
 
@@ -36,8 +47,14 @@ public:
         return tab[--atual];
     }
 
-    void print(ostream &output_stream, const char *msg)
+    void print(ostream &output_stream)
     {
+        output_stream << "[ ";
+        for (int i = 0; i < atual; i++)
+        {
+            output_stream << tab[i] << ((i != atual - 1) ? (", ") : (" "));
+        }
+        output_stream << "]";
     }
 
 private:
@@ -63,10 +80,12 @@ void print_assert(int success, const char *expression, const char *file, int lin
 
 int main()
 {
+    // ########################
     PilhaInt p1;
     p1.empilha(10);
     assert(p1.desempilha() == 10);
 
+    // ########################
     PilhaInt p2;
     p2.empilha(8);
     p2.empilha(9);
@@ -75,6 +94,50 @@ int main()
     assert(p2.desempilha() == 9);
     assert(p2.desempilha() == 8);
 
+    // ########################
+    PilhaInt p3;
+    p3 << 19 << 18 << 17;
+    p3 << 30;
+    stringstream buffer3;
+    buffer3 << p3.desempilha() << "," << p3.desempilha() << ","
+            << p3.desempilha() << "," << p3.desempilha() << endl;
+    assert(buffer3.str() == "30,17,18,19\n");
+
+    // #########################
+    PilhaInt p4;
+    stringstream buffer4;
+    p4 << 19 << 18 << 17 << 30;
+
+    p4.print(buffer4);
+    buffer4 << endl;
+    assert(buffer4.str() == "[ 19, 18, 17, 30 ]\n");
+
+    // #########################
+    PilhaInt p5;
+    p5 << 19 << 18 << 17 << 30;
+
+    stringstream ss;
+    p5.print(ss);
+    stringstream buffer5;
+
+    buffer5 << "{" << ss.str() << "}" << endl;
+    assert(buffer5.str() == "{[ 19, 18, 17, 30 ]}\n");
+
+    // ##########################
+    PilhaInt p6, q6;
+    q6 << 2;
+    p6 << 19 << 18 << 17 << 30;
+    q6 = p6;
+    p6.desempilha();
+    q6 << 7;
+    stringstream ssp, ssq;
+    p6.print(ssp);
+    q6.print(ssq);
+
+    stringstream buffer6;
+    buffer6 << "q = " << ssq.str() << "\n"
+            << "p = " << ssp.str() << endl;
+    assert(buffer6.str() == "q = [ 19, 18, 17, 30, 7 ]\np = [ 19, 18, 17 ]\n");
 }
 
 // ========================== End - Debug Session ==========================
